caa3ea7151c70e3af2ea447fe2d10d83ed8f94b7
andrbat <cs050771gap@gmail.com>
2022-02-02 11:15:24 +0200

    add list of films

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,10 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import { film } from "../types/type";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import GroupsIcon from "@mui/icons-material/Groups";
+import NoteAltOutlinedIcon from "@mui/icons-material/NoteAltOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "./home.css";
 
 interface HomeProps {
   curFilms: film[];
@@ -7,11 +12,41 @@ interface HomeProps {
 
 function Home({ curFilms }: HomeProps) {
   return (
-    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+    <Grid container spacing={2}>
       {curFilms.map((e) => (
-        <Grid item xs={4} key={e.id}>
-          <div>
-            <img src={"." + e.img} alt="" />
+        <Grid item xs={12} md={6} xl={4} key={e.id}>
+          <div className="home-film">
+            <img className="home-film_img" src={"." + e.img} alt="" />
+            <h2>{e.title}</h2>
+            <h3>
+              <GroupsIcon />
+              &nbsp;
+              {e.director}
+            </h3>
+            <h3>
+              <AccessTimeIcon />
+              &nbsp;
+              {e.duration}
+              &nbsp; min
+            </h3>
+            <div className="home-film_buttons">
+              <Button
+                variant="outlined"
+                size="medium"
+                startIcon={<NoteAltOutlinedIcon />}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="outlined"
+                size="medium"
+                color="error"
+                startIcon={<DeleteIcon />}
+              >
+                Delete
+              </Button>
+            </div>
+            <div className="home-film_price">{"$ " + e.price}</div>
           </div>
         </Grid>
       ))}
